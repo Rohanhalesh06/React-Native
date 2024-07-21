52dfd7e41446673b93be9bb86a3c181d55709913
Rohanhalesh06 <rohanhalesh@outlook.com>
2024-07-21 15:47:23 +0530

    test

--- detect.sh
+++ detect.sh
@@ -5,24 +5,20 @@ DIRECTORY_TO_WATCH="/home/rohan/React-Native"
 # Change to your Git repository directory if needed
 cd "$DIRECTORY_TO_WATCH" || exit
 
-# Function to stage changes if there are any
+# Function to stage all changes
 stage_changes() {
-    # Get a list of all modified files
-    local modified_files
-    modified_files=$(git status --porcelain | awk '{print $2}' | grep -v 'index.lock')
-
-    # Check if there are any modified files
-    if [ -n "$modified_files" ]; then
-        git add $modified_files
-        echo "Staged changes in files: $modified_files"
-    else
-        echo "No files to stage."
-    fi
+    git add .
+    echo "Staged all changes."
 }
 
-# Start monitoring directory
+# Start monitoring directory and filter out events for index.lock
 inotifywait -m -r -e modify,attrib,close_write,move,create,delete "$DIRECTORY_TO_WATCH" |
 while read -r path action file; do
+    # Skip if the file is index.lock
+    if [[ "$file" == "index.lock" ]]; then
+        continue
+    fi
+    
     echo "Detected change: $action on $file"
     stage_changes
 done
