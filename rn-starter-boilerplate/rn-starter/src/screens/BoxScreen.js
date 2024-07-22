8c41c8de899c3185c978bd9c2b49b5bc5499318b
Rohanhalesh06 <rohanhalesh@outlook.com>
2024-07-22 14:59:50 +0530

    Automated commit on 2024-07-22 14:59:47

--- rn-starter-boilerplate/rn-starter/src/screens/BoxScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/BoxScreen.js
@@ -3,32 +3,84 @@ import {View,Text,StyleSheet,Button} from 'react-native';
 
 const BoxScreen = function(){
     return(
-        <View style={styles.ViewStyle}>
-            <Text style={styles.TextOneStyle}>App</Text>
-            <Text style={styles.TextTwoStyle}>#child2</Text>
-            <Text style={styles.TextThreeStyle}>#child3</Text>
-            <Text style={styles.TextFourStyle}>#child4</Text>
-
+        <View style={styles.ParentStyle}>
+            <View style={styles.ViewOneStyle}/>
+            <View style={styles.ViewTwoStyle}/>
+            <View style={styles.ViewThreeStyle}/>
         </View>
     );
 }
 
 const styles=StyleSheet.create({
-    ViewStyle:{
+
+    ParentStyle:{
         borderWidth:5,
         borderColor:'Black',
+        height:200,
+        flexDirection:'row',
+        justifyContent:'space-between'
        // flexDirection:'row',
         //alignItems:'flex-start',
         //alignItems:'center',
         //alignItems:'flex-end'
        // justifyContent:'space-between',
-        //height:500,
+        //height:777,
+        //backgroundColor:"red"
 
     },
 
+    ViewOneStyle:{
+        //borderWidth:5,
+        //borderColor:'Black',
+       // flexDirection:'row',
+        //alignItems:'flex-start',
+        //alignItems:'center',
+        //alignItems:'flex-end'
+       // justifyContent:'space-between',
+        height:50,
+        width:50,
+        backgroundColor:"red"
+
+
+    },
+
+
+    ViewTwoStyle:{
+        //borderWidth:5,
+       // borderColor:'Black',
+       // flexDirection:'row',
+        //alignItems:'flex-start',
+        //alignItems:'center',
+        //alignItems:'flex-end'
+       // justifyContent:'space-between',
+       height:50,
+       width:50,
+       backgroundColor:"green",
+       alignSelf:'flex-end',
+       bottom:50
+
+
+    },
+
+
+    ViewThreeStyle:{
+        //borderWidth:5,
+        //borderColor:'Black',
+       // flexDirection:'row',
+        //alignItems:'flex-start',
+        //alignItems:'center',
+        //alignItems:'flex-end'
+       // justifyContent:'space-between',
+       height:50,
+       width:50,
+       backgroundColor:"purple"
+
+    },
+
+
     TextOneStyle:{
         borderWidth:5,
-        borderColor:'green',
+        borderColor:'black',
         textAlign:'center',
       //  margin:20,
        // marginHorizontal:20,
@@ -38,7 +90,7 @@ const styles=StyleSheet.create({
 
     TextTwoStyle:{
         borderWidth:5,
-        borderColor:'green',
+        borderColor:'red',
         alignSelf:'flex-start',
       //  margin:20,
        // marginHorizontal:20,
@@ -55,23 +107,16 @@ const styles=StyleSheet.create({
 
     TextThreeStyle:{
         borderWidth:5,
-        borderColor:'green',
+        borderColor:'purple',
       //  margin:20,
        // marginHorizontal:20,
         //marginVertical:20,
         //flex:4,
-       alignSelf:'flex-end'
+       alignSelf:'flex-end',
+       top:30,
+       position:'absolute'
     },
 
-    TextFourStyle:{
-        borderWidth:5,
-        borderColor:'green',
-      //  margin:20,
-       // marginHorizontal:20,
-        //marginVertical:20,
-        //flex:2
-        alignSelf:'center'
-    },
 
 });
 
