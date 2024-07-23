47fafe7e71d5328564fcc6778511fc7ee6062e72
Rohanhalesh06 <rohanhalesh@outlook.com>
2024-07-23 18:26:00 +0530

    Automated commit on 2024-07-23 18:25:56

--- FoodStore/food/src/screens/SearchScreen.js
+++ FoodStore/food/src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from "react";
 import {View,Text,Button,StyleSheet} from "react-native";
 import SearchBox from "./components/SearchBox";
 import useResults from "./components/hooks/useResults";
+import ResultsList from "./components/resultsList";
 
 const SearchScreen = function(){
     const [string,setstring] = useState('');
@@ -17,6 +18,13 @@ const SearchScreen = function(){
             />
             <Text>Search Screen</Text>
             <Text>we got {results.length} results</Text>
+            <ResultsList/>
+            <ResultsList/>
+
+            <ResultsList/>
+
+            <ResultsList/>
+
             {errorMessage ? <Text>{errorMessage}</Text> : null }
         </View>
     );
---
+++ FoodStore/food/src/screens/components/resultsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,StyleSheet,Text} from 'react-native';
+
+
+const ResultsList = function(){
+    return(
+        <View>
+            <Text>resultsList</Text>
+        </View>
+    );
+}
+
+
+export default ResultsList;
+
