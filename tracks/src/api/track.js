0d235e7990f73b66270ef2503a54696ae32716a8
Rohanhalesh06 <rohanhalesh@outlook.com>
2024-08-16 12:27:53 +0530

    Automated commit on 2024-08-16 12:27:50

--- tracks/src/api/track.js
+++ tracks/src/api/track.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const instance = axios.create({
-    baseURL:'https://9495-103-157-238-243.ngrok-free.app'
+    baseURL:'http://43.204.108.98:3001/'
 });
 
 instance.interceptors.request.use(
